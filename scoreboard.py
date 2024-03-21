1fb81d53f32684b4ad97eb03102d860274fa644d
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-03-21 16:07:21 +0000

    commit 8: editing boilerplate to make the code less cluttered

--- scoreboard.py
+++ scoreboard.py
@@ -24,8 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("data.txt", mode="w") as new_file:
-                new_file.write(str(self.high_score))
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
