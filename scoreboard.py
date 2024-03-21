074476d35bb7fd2ef595b0a190969c3270f84234
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-03-21 16:05:50 +0000

    commit 7: editing boilerplate to make the code less cluttered

--- scoreboard.py
+++ scoreboard.py
@@ -3,14 +3,14 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
-with open("data.txt", mode="r") as file:
-    contents = int(file.read())
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = contents
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
